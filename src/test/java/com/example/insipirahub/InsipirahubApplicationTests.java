8ca53edbd12eea87a6d2109d7f2a473bb2c5aa52
Joctan <perfect.altruistic@gmail.com>
2024-09-03 10:53:50 -0400

    maven files added

---
+++ src/test/java/com/example/insipirahub/InsipirahubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.insipirahub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsipirahubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
